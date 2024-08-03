92ce90601c14ef9d957a2e556c0be40e86337584
CaXaPCMeDoM <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-08-03 08:03:15 +0300

    first commit

---
+++ src/main/java/com/y_lab/y_lab/YLabApplication.java
@@ -0,0 +1,13 @@
+package com.y_lab.y_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YLabApplication.class, args);
+	}
+
+}
