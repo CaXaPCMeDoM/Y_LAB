4e9bf16fe8d0ac94aa2751132415e72e58019eb7
CaXaPCMeDoM <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-08-11 17:54:48 +0300

    style: deleting duplicates imports

--- src/test/java/com/y_lab/y_lab/repository/user/JdbcUserRepositoryTest.java
+++ src/test/java/com/y_lab/y_lab/repository/user/JdbcUserRepositoryTest.java
@@ -3,8 +3,6 @@ package com.y_lab.y_lab.repository.user;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class JdbcUserRepositoryTest {
 
     @BeforeEach
