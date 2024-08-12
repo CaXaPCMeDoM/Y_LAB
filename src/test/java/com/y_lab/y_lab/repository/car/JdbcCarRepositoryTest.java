c152a33adb1c8d62d5314530d800666a9d354c3c
CaXaPCMeDoM <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-08-12 08:19:04 +0300

    test: add JdbcCarRepositoryTest

--- src/test/java/com/y_lab/y_lab/repository/car/JdbcCarRepositoryTest.java
+++ src/test/java/com/y_lab/y_lab/repository/car/JdbcCarRepositoryTest.java
@@ -1,12 +1,24 @@
 package com.y_lab.y_lab.repository.car;
 
+import com.y_lab.y_lab.IntegrationEnvironment;
 import com.y_lab.y_lab.entity.Car;
 import com.y_lab.y_lab.entity.enums.CarState;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,25 +27,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
-public class JdbcCarRepositoryTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16")
-            .withDatabaseName("db")
-            .withUsername("caxap")
-            .withPassword("1234");
-
-    private static Connection connection;
+public class JdbcCarRepositoryTest extends IntegrationEnvironment {
     private static JdbcCarRepository carRepository;
 
     @BeforeAll
     public static void setUp() throws Exception {
-        // Устанавливаем соединение с БД
-        connection = DriverManager.getConnection(
-                postgresContainer.getJdbcUrl(),
-                postgresContainer.getUsername(),
-                postgresContainer.getPassword()
-        );
         carRepository = new JdbcCarRepository(connection);
     }
 
@@ -43,37 +41,48 @@ public class JdbcCarRepositoryTest {
     }
 
     @Test
-    public void testAddCar() {
+    public void addCar() {
         Car car = new Car(null, "Toyota", "Corolla", 2020, 20000.0, CarState.NEW);
         Long carId = carRepository.add(car);
         assertNotNull(carId);
 
         List<Car> cars = carRepository.findAll();
-        assertEquals(1, cars.size());
-        assertEquals("Toyota", cars.get(0).getBrand());
+        assertEquals(cars.get(cars.size() - 1).getCarId(), carId);
     }
 
     @Test
-    public void testDeleteCar() {
+    public void deleteCar() {
         Car car = new Car(null, "Honda", "Civic", 2019, 18000.0, CarState.USED);
         Long carId = carRepository.add(car);
+        List<Car> carsBefore = carRepository.findAll();
 
         Car deletedCar = carRepository.delete(carId);
         assertNotNull(deletedCar);
         assertEquals(carId, deletedCar.getCarId());
 
-        List<Car> cars = carRepository.findAll();
-        assertTrue(cars.isEmpty());
+        List<Car> carsAfter = carRepository.findAll();
+        assertEquals(carsBefore.size(), carsAfter.size() + 1);
     }
 
     @Test
-    public void testFindByBrand() {
-        Car car1 = new Car(null, "Toyota", "Camry", 2021, 25000.0, CarState.NEW);
-        Car car2 = new Car(null, "Toyota", "Corolla", 2020, 20000.0, CarState.NEW);
-        carRepository.add(car1);
-        carRepository.add(car2);
+    public void findByBrand() {
+        Car carFirst = new Car(null, "Toyota", "Camry", 2021, 25000.0, CarState.NEW);
+        Car carSecond = new Car(null, "Toyota", "Corolla", 2020, 20000.0, CarState.NEW);
+        Long carFirstId = carRepository.add(carFirst);
+        Long carSecondId = carRepository.add(carSecond);
 
         List<Car> toyotaCars = carRepository.findByBrand("Toyota");
-        assertEquals(2, toyotaCars.size());
+        boolean flagFirst = false;
+        boolean flagSecond = false;
+        for (Car car : toyotaCars) {
+            if (carFirstId.equals(car.getCarId())) {
+                flagFirst = true;
+            }
+            if (carSecondId.equals(car.getCarId())) {
+                flagSecond = true;
+            }
+        }
+        assertTrue(flagFirst);
+        assertTrue(flagSecond);
     }
 }
